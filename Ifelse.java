415ea137d7134ade6697b0de95cd1bf0878beb6c
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-11-26 22:07:09 +0600

    Update Ifelse.java

--- Ifelse.java
+++ Ifelse.java
@@ -7,7 +7,7 @@ public class Ifelse {
 
         boolean b1 = a == b;
         boolean b2 = d.equals(b + "!");
-        boolean b3 = !c.equals(d);
+        boolean b3 = !c.equals(a);
 
         if (b1 && b2 && b3) {
             System.out.println("Success!");
