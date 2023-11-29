b10c7404f9cf2a767a846a03e8a94569897e0fc4
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-11-29 22:31:16 +0600

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,1 +1,12 @@
+public class Methods {
+  static void myMethod(String fname) {
+    System.out.println(fname + " Zhuma");
+  }
+
+  public static void main(String[] args) {
+    myMethod("Abay");
+    myMethod("Alan");
+    myMethod("Aiho");
+  }
+}
 
