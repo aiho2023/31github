202b4f6edb4804e1945aece0cd800058732dab3a
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-01 22:28:01 +0600

    Update Objects.java

--- Objects.java
+++ Objects.java
@@ -3,7 +3,7 @@ public class Objects{
     public static void main(String[] args) {
              
         Person Aika = new Person("Aika");
-        System.out.println(Aika.toString()); // Person Aika
+        System.out.println(Aika.hashCode()); // Person Aika
     }
 }
 class Person {
