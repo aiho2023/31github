cd76dd26de0f86cdab5ec16a8a2f51a9565d6f46
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-01 22:12:59 +0600

    Update Objects.java

--- Objects.java
+++ Objects.java
@@ -1,1 +1,22 @@
-
+public class Objects{
+      
+    public static void main(String[] args) {
+             
+        Person tom = new Person("Aika");
+        System.out.println(tom.toString()); // Person Tom
+    }
+}
+class Person {
+     
+    private String name;
+    
+    public Person(String name){
+     
+        this.name=name;
+    }
+     
+    public String toString(){
+          
+        return "Person " + name;
+    }
+}
