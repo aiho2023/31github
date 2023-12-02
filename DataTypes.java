2a2cef3b8f358eb19039e0cae397fdcf964c15fd
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-02 23:43:28 +0600

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -2,7 +2,10 @@ public class DataTypes{
       
     public static void main(String[] args) {
              
-        String text = "Hello \nworld";
+        SString text = "Вот мысль, которой весь я предан,\n"+
+                "Итог всего, что ум скопил.\n"+
+                "Лишь тот, кем бой за жизнь изведан,\n"+
+                "Жизнь и свободу заслужил.";
         System.out.println(text); 
     }
 }
