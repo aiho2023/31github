f2de0f9b0bb6f44a68ffe90775d87c75b6f179bb
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-03 22:59:37 +0600

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
     public static void main(String [] args) {
        int i;
-for (int i = 0; i < numbers.length; i++) {
+for (int i = 0; i < 3; i++) {
             int el = numbers[i];
     if (el >= 3) {
         break;
     }
     System.out.println(el);
-    if (i >= 1) {
+    if (el >= 1) {
         continue;
     }
     if (el % 3 == 0) {
