34f98abd5ff5da2ad2fcd7b7f59329fca17f7266
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-03 23:37:38 +0600

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-       for (int i = 0; i < 3; i++) {
-            if (i >= 3) {
+       for (int i = 0; i < 4; i++) {
+            if (i >= 4) {
         break;
     }
     System.out.println(i);
