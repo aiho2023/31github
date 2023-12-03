b92f489905b4d3ff48a15d89405bc2229a6881f2
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-03 22:40:16 +0600

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,1 +1,14 @@
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+       int i;
+for (i = 0; i < 5; i++) {
+    if (i >= 3) {
+        break;
+    }
+    System.out.println("Yuhu");
+    if (i >= 1) {
+        continue;
+    }
+    System.out.println("Tata"); 
+    }
 
