9652be7c2b5a87c0387d1ec3a9ef2172fbb4729e
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-05 23:03:45 +0600

    Update and rename FizzBuzz3.java to FizzBuzz2.java

---
+++ FizzBuzz2.java
@@ -0,0 +1,15 @@
+public class FizzBuzz2 {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6};
+        
+        for (int i = 0; i < numbers.length; i++) {
+            int el = numbers[i];
+          if (el>=1); { 
+            System.out.println(el); 
+             } 
+         else (el%3==0);{ 
+           System.out.println("Fizz");
+         }
+    }
+}
+}    
