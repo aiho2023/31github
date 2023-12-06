a430523708fedfb382d5052d9868f5dc2bc168b0
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-06 22:20:55 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,13 +1,9 @@
 public class MaximumSubarray {
    public static void main(String[] args); 
-   public static int[] copyArray(){
-       int[] numbers = {1,2,3,4,5,6,7};
-       int[] subArray = new int[numbers.length-3];
-       int j =3;
-       for (int i=0;i<subArray.length;i++){
-           subArray[i] = numbers[j+i];
-       }
-       System.out.println(Arrays.toString(subArray));
-       return subArray;
-   }
+   static int[] copyArray1(){
+  int[] numbers = {1,2,3,4,5,6,7};
+  int[] subArray = Arrays.copyOfRange(numbers,3,numbers.length);
+  System.out.println(Arrays.toString(subArray));
+  return subArray;
+}
 }
