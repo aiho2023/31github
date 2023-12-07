b8ebb6c8cd4d14840331810fdafe872901a7ee29
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-07 19:52:29 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -8,7 +8,7 @@ public class MaximumSubarray {
         System.out.println();
     }
 
-    public static void main(String[] args) {
+    public static void MaximumSubarray(String[] args) {
         int[] array = {1,2,3,4,5};
         System.out.print("Original Array is - [ ");
         for(int i:array) System.out.print( i + " ");
