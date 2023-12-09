165a8c2f31a0003f4427c9fba7d08f98f90c69e3
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-09 22:25:55 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,8 +1,8 @@
 public class MaximumSubarray {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3};
-        Int[] partialNumbers = Arrays.copyOfRange(numbers, 0, 1); 
-Int[] partialNumbers = ArrayUtils.subarray(numbers, 0, 1);
+        Int[] MaximumSubarray = Arrays.copyOfRange(numbers, 0, 1); 
+Int[] MaximumSubarray = ArrayUtils.subarray(numbers, 0, 1);
 List<Int> NumbersList = Arrays.asList( Arrays.copyOfRange(numbers, 0, 1) );
         
         System.out.println(NumbersList);
