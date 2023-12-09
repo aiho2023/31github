c20dc51e274b867822cc91757dc20336ae57d6ba
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-09 21:54:48 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,23 +1,10 @@
-public class main {
-
-    private static void subArray(int[] array, int startIndex, int endIndex){
-        int[] result = ArrayUtils.subarray(array, startIndex, endIndex);
-        System.out.print("SubArray for " + startIndex + " and " + endIndex + " is [ ");
-        for(int i: result) System.out.print(i + " ");
-        System.out.print("]");
-        System.out.println();
-    }
-
+public class MaximumSubarray {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5};
-        System.out.print("Original Array is - [ ");
-        for(int i:array) System.out.print( i + " ");
-        System.out.println("]");
-        subArray(array, 0, 3);
-        subArray(array, -1, 3);
-        subArray(array, 10, 20);
-        subArray(array, 0, -5);
-        subArray(array, 0, 10);
+        int[] numbers = {1, 2, 3};
+        int length = numbers[2];
+        char[] chars = new char[length];
+        chars[numbers.length-1] = 'a';
+        System.out.println("Done!");
     }
 }
 
