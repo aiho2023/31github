cde4877862c99a63ad161e1e6e7d226829e224d7
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-09 22:41:32 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,6 +1,6 @@
 public class MaximumSubarray {
     public static void main(String[] args) {
-       String[] arr = names {"Alex", "Brian", "Charles", "David"};;
+       String[] arr = names {"Alex", "Brian", "Charles", "David"};
        
 //Subarray from index '0' (inclusive) to index '2' (exclusive)
 String[] partialNames = Arrays.copyOfRange(names, 0, 2);  // [Alex, Brian]
