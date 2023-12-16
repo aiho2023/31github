a519618c9b08f8cffb4d03e4dee010af09d02222
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-16 23:06:51 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -18,7 +18,7 @@ public class MaximumSubarray {
                         System.out.println(
                             "Sum found between indexes " + i
                             + " and " + j);
-                        System.out.println(currentSum);
+                        System.out.println(int n);
                         return;
                     }
                 }
