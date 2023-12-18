0842cef11650beb8a42a1462e795dbeb113e5df0
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-18 23:16:48 +0600

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -26,9 +26,9 @@ public class MaximumSubarray {
         System.out.println("No subarray found");
         return;
     }
- 
+ import java.util.Arrays;
     public static void main(String[] args)
-    {   import java.util.Arrays;
+    {   
         MaximumSubarray arraysum = new MaximumSubarray();
         int arr[] = { -2,1,-3,4,-1,2,1,-5,4 };
         int n = arr.length;
