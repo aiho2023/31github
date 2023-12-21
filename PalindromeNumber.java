3e40b6c38ffe3c3be1d43825975d63689ac309dc
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-21 22:35:55 +0600

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,1 +1,10 @@
+function solution(x) {
+if (x===x.split("").reverse().join("")) { // проверяет палиндром или нет
+    return true 
+}
+for (var i = 0; i < x.length; i++) {
+    //тут думаю надо поменять местами элементы и проверять заново,но как не знаю (
+} } 
 
+
+console.log(solution("aabb")) // true
