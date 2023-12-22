32ac8a59c1e0aefdd1c78368274703494a071575
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-22 23:21:21 +0600

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -2,9 +2,9 @@ public class PalindromeNumber{
 public static void main(String[] args){
 var X = 121;
 if (X.toString().split("").reverse().join("") == X.toString()) {
-    console.log("Это палиндром")
+    console.log("Это палиндром");
 } else {
-    console.log("Это не палиндром")
+    console.log("Это не палиндром");
 }
 }
 }
