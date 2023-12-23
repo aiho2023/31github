96221f463cd28287c37d1868376cab4c4ae1b908
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-23 23:15:29 +0600

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,16 +1,16 @@
 public class PalindromeNumber{
 public static void main(String[] args){
-    function solution(str){
-    let chars = new Set(str),
-        wasOdd = false;
-
-    for(let char of chars)
-        if(str.match(new RegExp(char, 'gi')).length % 2)
-            if(wasOdd)
-                return false;
-            else
-                wasOdd = true;
-    return true;
+   number = int(input("Введите число: "))
+number_str = str(number)
+is_palindrome = True
+for i in range(len(number_str) // 2):
+if number_str[i] != number_str[-i - 1]:
+is_palindrome = False
+break
+if is_palindrome:
+print("Число является палиндромом.")
+else:
+print("Число не является палиндромом.")
 }
 }
 }
