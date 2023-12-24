ab01c59c645718de1d4939b53e7eb84677fb1786
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-24 23:20:13 +0600

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,16 +1,13 @@
-public class PalindromeNumber{
-public static void main(String[] args){
-   number = int(input("Введите число: "))
-number_str = str(number)
-is_palindrome = True
-for i in range(len(number_str) // 2):
-if number_str[i] != number_str[-i - 1]:
-is_palindrome = False
-break
-if is_palindrome:
-System.out.println("Число является палиндромом.")
-else
-System.out.println("Число не является палиндромом.")
-}
-}
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(10101)); // => true
+        System.out.println(isPalindrom(10)); // => false
+    }
+
+    public static boolean isPalindrom(int number) {
+        StringBuilder builder = new StringBuilder();
+        String str = builder.append(number).reverse().toString();
+        int reversedNumber = Integer.parseInt(str);
+        return number == reversedNumber;
+    }
 }
