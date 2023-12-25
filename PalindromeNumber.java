9055dd926ecdb1ab9d22c789a2306b9ab634a083
Aiho <151383696+aiho2023@users.noreply.github.com>
2023-12-25 23:55:49 +0600

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,7 +1,7 @@
 public class PalindromeNumber {
     public static void main(String[] args) {
-        System.out.println(isPalindrom(101)); // => true
-        System.out.println(isPalindrom(11911)); // => false
+        System.out.println(isPalindrom(1b1)); // => true
+        System.out.println(isPalindrom(aabb)); // => false
     }
 
     public static boolean isPalindrom(int number) {
